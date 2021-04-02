4a7f7a488d5e34f1f4d0c966f97807edb0842ae5
RodrigoBessa85 <rbessa_analista@hotmail.com>
2021-04-01 23:26:51 -0300

    Implementação dos métodos em categorias.

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../shared/category.module';
+import { CategoryService } from '../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
 
-  ngOnInit(): void {
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista.')
+    )
+  }
+
+  deleteCategory(category) {
+    const mustDelete = confirm("Deseja realmente excluir esse item?");
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert("Erro ao tentar excluir.")
+      )
+    }
   }
 
 }
