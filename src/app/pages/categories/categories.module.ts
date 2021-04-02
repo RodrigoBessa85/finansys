2bfc095af2ac1ce71e6a4d76d0a9071be91b9d91
RodrigoBessa85 <rbessa_analista@hotmail.com>
2021-04-02 00:31:56 -0300

    Add e implementação da tela de Formulário.

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryFormComponent } from './category-form/category-form.component';
 import { CategoryListComponent } from './category-list/category-list.component';
@@ -8,6 +10,7 @@ import { CategoryListComponent } from './category-list/category-list.component';
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     CategoriesRoutingModule
   ]
 })
